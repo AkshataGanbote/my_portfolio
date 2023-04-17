7c52430dcb6331dbb466526849525085afe2fdd1
Akshata Ganbote <117456092+AkshataGanbote@users.noreply.github.com>
2023-04-17 14:29:48 +0530

    Updated Portfolio

---
+++ src/js/main.js
@@ -0,0 +1,25 @@
+// style
+import '../sass/main.scss';
+
+// navbar
+import { Collapse } from 'bootstrap';
+
+// scroll spy
+import { ScrollSpy } from 'bootstrap';
+
+// dark/light mode
+import './themes';
+
+// typed js
+import './typed';
+
+
+// from functions
+import './contact';
+
+// scroll animation
+import AOS from 'aos';
+import '../../node_modules/aos/dist/aos.css';
+AOS.init({
+  once: true
+});
---
+++ src/js/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["a Front-End Developer!", "a MERN Stack Developer!"],
+  typeSpeed: 120,
+  backSpeed: 50,
+  backDelay: 1500,
+  loop: true
+};
+
+var typed = new Typed('#headline', options);
